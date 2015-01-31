d86339f2b866a73ed1725819c368dfcf6103771d
Bystroushaak <bystrousak@kitakitsune.org>
2015-02-01 00:00:55 +0100

    #1: Added tests of different methods, no attribute and multiple attributes.

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -6,6 +6,8 @@
 # Imports =====================================================================
 import sys
 
+import pytest
+
 sys.path.insert(0, "src")
 sys.path.insert(0, "../src")
 
@@ -30,3 +32,29 @@ def test_http_url():
 
     assert p.get() == ("get", "http://kitakitsune.org", None)
     assert p.attr.get() == ("get", "http://kitakitsune.org/attr", None)
+
+
+def test_other_methods():
+    p = ReturnWrapper("http://kitakitsune.org")
+
+    assert p.post() == ("post", "http://kitakitsune.org", None)
+    assert p.update() == ("update", "http://kitakitsune.org", None)
+
+    assert p.attr.post() == ("post", "http://kitakitsune.org/attr", None)
+    assert p.attr.update() == ("update", "http://kitakitsune.org/attr", None)
+
+
+def test_no_method_given():
+    p = ReturnWrapper("http://kitakitsune.org")
+
+    with pytest.raises(ValueError):
+        assert p() == ("a", "http://kitakitsune.org", None)
+
+
+def test_multiple_attributes():
+    p = ReturnWrapper("http://kitakitsune.org")
+
+    assert p.a() == ("a", "http://kitakitsune.org", None)
+    assert p.b.a() == ("a", "http://kitakitsune.org/b", None)
+    assert p.c.b.a() == ("a", "http://kitakitsune.org/c/b", None)
+    assert p.d.c.b.a() == ("a", "http://kitakitsune.org/d/c/b", None)
