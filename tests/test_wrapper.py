4b41c45c46644b94732e716d43ad5ad237aa15ee
Bystroushaak <bystrousak@kitakitsune.org>
2015-01-31 23:54:41 +0100

    #1: PrintWrapper changed to ReturnWrapper. Added test_http_url().

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -12,13 +12,21 @@ sys.path.insert(0, "../src")
 from attribute_wrapper import GenericWrapper
 
 
-# Tests =======================================================================
-class PrintWrapper(GenericWrapper):
+# Classes =====================================================================
+class ReturnWrapper(GenericWrapper):
     def download_handler(self, method, url, data):
-        return "%s:%s:%s" % (method, url, str(data))
+        return (method, url, data)
 
 
+# Tests =======================================================================
 def test_wrapper():
-    p = PrintWrapper("print")
+    p = ReturnWrapper("return")
 
-    assert p.attribute.get() == "get:print/attribute:None"
+    assert p.attribute.get() == ("get", "return/attribute", None)
+
+
+def test_http_url():
+    p = ReturnWrapper("http://kitakitsune.org")
+
+    assert p.get() == ("get", "http://kitakitsune.org", None)
+    assert p.attr.get() == ("get", "http://kitakitsune.org/attr", None)
